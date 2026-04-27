a847633aa9c926653665868abeb24c20d8eb6255
Rubens Takashi Maruoka Vieira <rtmv.snf23@uea.edu.br>
2026-04-27 01:24:27 -0400

    readme and fixes

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from src import ingestion
-
-def main():
-    print("Hello from micro-saas-com-rag!")
-    ingestion.main()
-
+from src.pipeline import *
 
 if __name__ == "__main__":
-    main()
+    from src.ingestion import load_pdfs
+    from src.chunking import create_chunks
+    from src.embeddings import index_chunks
+    from src.config import DATA_PATH
+
+    print("Carregando os PDFs...")
+    docs = load_pdfs(DATA_PATH)
+    print("Criando os chunks...")
+    chunks = create_chunks(docs)
+    print("Indexando no vectorstore...")
+    index_chunks(chunks)
+    print("Pipeline finalizada!")
